09c844a73f01967b545ebddf6bea75cb7579c244
luv4code-lc <madhav.p.akv@gmail.com>
2022-02-23 16:51:05 +0530

    creating default project structure. closes #1

---
+++ src/test/java/com/luv4code/brewery/BreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv4code.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
